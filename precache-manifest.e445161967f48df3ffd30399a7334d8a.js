6e85a78725664c4cc7109aa6b92c476b4dcc8bc4
25518 <zhiwei.deng@hand-china.com>
2019-07-24 17:35:26 +0800

    Updates

---
+++ precache-manifest.e445161967f48df3ffd30399a7334d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727d3541ae5b693d909d21f320616221",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "6c671bcd18abd70f7f13",
+    "url": "/todolist/static/css/main.5b58fd20.chunk.css"
+  },
+  {
+    "revision": "60cb671d023c2743737a",
+    "url": "/todolist/static/js/2.56d428b5.chunk.js"
+  },
+  {
+    "revision": "6c671bcd18abd70f7f13",
+    "url": "/todolist/static/js/main.5fa00976.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
